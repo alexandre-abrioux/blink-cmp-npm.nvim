6d21b3c56527ad8630cacfa8ae1803d30064c34f
Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>
2025-05-18 03:33:14 +0200

    feat: match version based on cursor (#7)

--- lua/blink-cmp-npm/utils/compute_meta.lua
+++ lua/blink-cmp-npm/utils/compute_meta.lua
@@ -4,6 +4,7 @@ local function compute_meta(line, ctx)
   -- protect regex performance
   assert(#line <= 200)
 
+  local col = ctx.cursor[2]
   local name = line:match('%s*"([^"]*)"?')
   if name == nil then
     return { line, nil, nil, nil, nil, nil, nil, nil, nil, nil }
@@ -18,28 +19,27 @@ local function compute_meta(line, ctx)
   local find_version = false
 
   if pos_end_name then
-    local _, pos_second_quote_find = line:find('"', pos_end_name and pos_end_name + 1 or 1, true)
+    local _, pos_second_quote_find = line:find('"', pos_end_name + 1, true)
     pos_second_quote = pos_second_quote_find
   end
 
   if pos_second_quote then
-    local _, pos_third_quote_find = line:find('"', pos_second_quote and pos_second_quote + 1 or 1, true)
+    local _, pos_third_quote_find = line:find('"', pos_second_quote + 1, true)
     pos_third_quote = pos_third_quote_find
   end
 
   if pos_third_quote then
-    local _, pos_fourth_quote_find = line:find('"', pos_third_quote and pos_third_quote + 1 or 1, true)
+    local _, pos_fourth_quote_find = line:find('"', pos_third_quote + 1, true)
     pos_fourth_quote = pos_fourth_quote_find
   end
 
-  if pos_third_quote and pos_fourth_quote and pos_fourth_quote - pos_third_quote > 1 then
-    current_version = line:match('.*".*".*"[~^]?(.*)"')
-    current_version_matcher = line:match('.*".*".*"([~^]?).*"')
+  if pos_third_quote then
+    find_version = col >= pos_third_quote
   end
 
-  if pos_third_quote then
-    local col = ctx.cursor[2]
-    find_version = col >= pos_third_quote
+  if find_version then
+    local line_up_to_cursor = line:sub(0, col)
+    current_version_matcher, current_version = line_up_to_cursor:match('^[^"]*"[^"]*"[^"]*"([~^]?)([^"]*)"?$')
   end
 
   return {
