f911aca57ff2d97d0e4f0473d0090f356f038ffe
Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>
2026-08-30 11:15:55 +0200

    fix: lock test dependencies

--- minit.lua
+++ minit.lua
@@ -1,12 +1,13 @@
 #!/usr/bin/env -S nvim -l
 
 vim.env.LAZY_STDPATH = ".tests"
-load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua"))()
+load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/refs/tags/v11.17.5/bootstrap.lua"))()
 
 -- Setup lazy.nvim
 require("lazy.minit").busted({
   spec = {
     "nvim-treesitter/nvim-treesitter",
+    commit = "cf12346a3414fa1b06af75c79faebe7f76df080a",
   },
   rocks = {
     enabled = false,
